cda2df42c37adbd3824b054c85d3be6a94b793e8
Anthony Loya <53438741+AnthonyLoya913@users.noreply.github.com>
2023-01-16 20:06:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -241,71 +241,79 @@ st.text("")
 
 c29, c30, c31 = st.columns([1, 1, 2])
 
-st.title('Enter payment info:')
-st.text('Files are $5.00 USD per download')   
+with c29:
 
-def is_valid_email(email):
-    return re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'+"", email)
+    CSVButton = download_button(
+        df,
+        "File.csv",
+        "Download to CSV",
+    )
 
-email = st.text_input(label='Email', value='', key='email_input')
+# st.title('Enter payment info:')
+# st.text('Files are $5.00 USD per download')   
 
-if email.strip() == "":
-    st.error("Email field cannot be empty.")
-elif not is_valid_email(email):
-    st.error("Invalid email address.")
+# def is_valid_email(email):
+#     return re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'+"", email)
 
-number = st.text_input(label='Card Number', value='**** **** **** ****', key='number_input')
-exp_month = st.text_input(label='Expiration Month', value='**', key='exp_month_input')
-exp_year = st.text_input(label='Expiration Year', value='**', key='exp_year_input')
-cvc = st.text_input(label='CVC', value='***', key='cvc_input')
+# email = st.text_input(label='Email', value='', key='email_input')
 
-deta = Deta("b010f1vs_EG3yHoWib22swGdRWRBdRSDanD7qqGTD")
-emails = deta.Base("emails")
+# if email.strip() == "":
+#     st.error("Email field cannot be empty.")
+# elif not is_valid_email(email):
+#     st.error("Invalid email address.")
 
-def handle_payment():
-    if not all([email, number, exp_month, exp_year, cvc]):
-        return False
-    try:
-        token = stripe.Token.create(
-            card={
-                "number": number,
-                "exp_month": exp_month,
-                "exp_year": exp_year,
-                "cvc": cvc
-            }
-        )
-    except stripe.error.InvalidRequestError as e:
-        st.error("Error: {}".format(e))
-        return False
-    except Exception as e:
-        st.error("Error: {}".format(e))
-        return False
-    try:
-        charge = stripe.Charge.create(
-            amount=500,  # charge amount in cents
-            currency='usd',
-            description='CSV Download',
-            source=token["id"],
-        )
-        if charge["status"] == "succeeded":
-            try:
-                emails.insert({"email": email})
-            except Exception as e:
-                st.error("Error inserting email: {}".format(e))
-                return False
-        return True
-    except stripe.error.CardError as e:
-        st.error("Error: {}".format(e))
-        return False
+# number = st.text_input(label='Card Number', value='**** **** **** ****', key='number_input')
+# exp_month = st.text_input(label='Expiration Month', value='**', key='exp_month_input')
+# exp_year = st.text_input(label='Expiration Year', value='**', key='exp_year_input')
+# cvc = st.text_input(label='CVC', value='***', key='cvc_input')
+
+# deta = Deta("b010f1vs_EG3yHoWib22swGdRWRBdRSDanD7qqGTD")
+# emails = deta.Base("emails")
+
+# def handle_payment():
+#     if not all([email, number, exp_month, exp_year, cvc]):
+#         return False
+#     try:
+#         token = stripe.Token.create(
+#             card={
+#                 "number": number,
+#                 "exp_month": exp_month,
+#                 "exp_year": exp_year,
+#                 "cvc": cvc
+#             }
+#         )
+#     except stripe.error.InvalidRequestError as e:
+#         st.error("Error: {}".format(e))
+#         return False
+#     except Exception as e:
+#         st.error("Error: {}".format(e))
+#         return False
+#     try:
+#         charge = stripe.Charge.create(
+#             amount=500,  # charge amount in cents
+#             currency='usd',
+#             description='CSV Download',
+#             source=token["id"],
+#         )
+#         if charge["status"] == "succeeded":
+#             try:
+#                 emails.insert({"email": email})
+#             except Exception as e:
+#                 st.error("Error inserting email: {}".format(e))
+#                 return False
+#         return True
+#     except stripe.error.CardError as e:
+#         st.error("Error: {}".format(e))
+#         return False
         
-with st.form(key='my_form'):
-    submit_button  = st.form_submit_button(label='Submit Payment', on_click=handle_payment)
-    if submit_button:
-        payment_success = handle_payment()
-        if payment_success:
-            st.success("Your payment of $5.00 USD was successful!")
-            CSVButton = download_button(
-                df,
-                file_name.split(".")[0]+".csv",
-                "Download to CSV",
-                )
+# with st.form(key='my_form'):
+#     submit_button  = st.form_submit_button(label='Submit Payment', on_click=handle_payment)
+#     if submit_button:
+#         payment_success = handle_payment()
+#         if payment_success:
+#             st.success("Your payment of $5.00 USD was successful!")
+#             CSVButton = download_button(
+#                 df,
+#                 file_name.split(".")[0]+".csv",
+#                 "Download to CSV",
+#                 )
